a5e8354114cc9f3db4165e6b9d772f39a2f11d59
Klaus Lyko <lyko.klaus@gmail.com>
2012-04-25 16:01:41 +0200

    definition of webservice

--- src/de/uni_leipzig/simba/limeswebservice/service/LimesService.java
+++ src/de/uni_leipzig/simba/limeswebservice/service/LimesService.java
@@ -1,5 +1,31 @@
 package de.uni_leipzig.simba.limeswebservice.service;
 
-public interface LimesService {
+import java.util.HashMap;
 
+public interface LimesService {
+	/**
+	 * 
+	 * @param source HashMap <table>
+	 * 	<tr><td>endpoint</td><td>String</td></tr>
+	 *  <tr><td>graph</td><td>String</td></tr>
+	 *  <tr><td>prefix</td><td>HashMap "<"String, String">"</td></tr>
+	 *  <tr><td>properties</td><td>HashMap "<"String, String">"</td></tr>
+	 *  <tr><td>var</td><td>String</td></tr>
+	 *  <tr><td></td><td></td></tr>
+	 *  </table>
+	 * @param targete HashMap <table>
+	 * 	<tr><td>endpoint</td><td>String</td></tr>
+	 *  <tr><td>graph</td><td>String</td></tr>
+	 *  <tr><td>prefix</td><td>HashMap "<"String, String">"</td></tr>
+	 *  <tr><td>properties</td><td>HashMap "<"String, String">"</td></tr>
+	 *  <tr><td>var</td><td>String</td></tr>
+	 *  <tr><td></td><td></td></tr>
+	 *  </table>
+	 * @param metrice HashMap <table>
+	 *  <tr>metric<td></td><td>String</td></tr>
+	 *  <tr>accthreshold<td></td><td>Double</td></tr>
+	 *  <tr>verthreshold<td></td><td>Double</td></tr>
+	 *  </table>
+	 */
+	public void getMapping(HashMap<String, Object> source, HashMap<String, Object> target, HashMap<String, Object> metric);
 }
