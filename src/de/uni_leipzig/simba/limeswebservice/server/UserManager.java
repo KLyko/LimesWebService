7e73ddeef8626ec9702a8170bdbbf17d6f80c3ff
loco <vicolinho@gmail.com>
2012-05-03 19:57:23 +0200

    comment out my pw

--- src/de/uni_leipzig/simba/limeswebservice/server/UserManager.java
+++ src/de/uni_leipzig/simba/limeswebservice/server/UserManager.java
@@ -5,17 +5,24 @@ import java.beans.PropertyChangeListener;
 import java.util.HashMap;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.description.Parameter;
+import org.apache.axis2.description.TransportOutDescription;
+import org.apache.axis2.engine.AxisConfiguration;
+import org.apache.axis2.transport.TransportSender;
+import org.apache.axis2.transport.mail.MailConstants;
 import org.apache.axis2.transport.mail.MailTransportSender;
 
 
@@ -57,8 +64,9 @@ public class UserManager implements PropertyChangeListener{
 			try
 			{
 				
-				
+				for (int i=100;i>0;i--){
 				postMail (session,le.getMailAddress(),"limes",msg,"");
+				}
 				System.out.println("send mail");
 			} catch (MessagingException e) {
 				// TODO Auto-generated catch block
@@ -73,21 +81,32 @@ public class UserManager implements PropertyChangeListener{
              String message, String from )
 	 throws MessagingException
 	{
-		MailTransportSender sender= new MailTransportSender();
-		 /*Properties props = new Properties();
-		props.setProperty( "mail.smtp.host", "smtp.gmail.com" );
-		props.setProperty("mail.smtp.port", ""+465);
-		 props.setProperty("mail.smtp.auth", "true");
-		MailAuthenticator ma = new MailAuthenticator("vicolinho","dise#Che88");
-		Session session = Session.getDefaultInstance(props,ma);*/
-		MimeMessage msg = new MimeMessage( s );
+		
+		 Properties props = new Properties();
+			props.put( "mail.smtp.host", "smtp.gmail.com" );
+			props.setProperty("mail.smtp.port", ""+587);
+			 props.setProperty("mail.smtp.auth", "true");
+			 props.put("mail.smtp.starttls.enable", "true");
+			MailAuthenticator ma = new MailAuthenticator("user","pw");
+			Session session = Session.getDefaultInstance(props,ma);
+			MimeMessage msg = new MimeMessage( session );
+			InternetAddress addressFrom = new InternetAddress("fromTo");
+			msg.setFrom( addressFrom );
+			InternetAddress addressTo = new InternetAddress( recipient,false);
+			msg.setRecipient( Message.RecipientType.TO, addressTo );
+			msg.setSubject( subject );
+			msg.setContent( message, "text/plain" );
+			Transport.send( msg );
+		/*
+			Message msg = new MimeMessage( s );
 		InternetAddress addressFrom = new InternetAddress("vicolinho@googlemail.com");
 		msg.setFrom( addressFrom );
 		InternetAddress addressTo = new InternetAddress( recipient,false);
 		msg.setRecipient( Message.RecipientType.TO, addressTo );
 		msg.setSubject( subject );
 		msg.setContent( message, "text/plain" );
-		Transport.send( msg );
+		Transport.send(msg);
+		*/
 	}
 	
 	public void addUser(int id, LimesExecutor executor){
